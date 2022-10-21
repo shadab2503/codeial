95623c7b96291de069a1466eb3a3e62839847b07
shadab2503 <shadab.gazipur@gmail.com>
2022-10-21 20:11:32 +0530

    setup static files access for layout and pages

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 router.get('/',homeController.home);
+
+router.use('/users',require('./users'));
 // console.log('router is loaded');
 
 module.exports = router;
