4711bc487cf77981343bee8ea1b993de3dd26888
shadab2503 <shadab.gazipur@gmail.com>
2022-10-22 00:45:21 +0530

    upto sign up

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
 
+
+const db = require('./config/mongoose')
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
